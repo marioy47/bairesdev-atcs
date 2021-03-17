22f74e8c706054575e07d31fe1f0f1b284e228e5
Mario Yepes <marioy47@gmail.com>
2021-03-17 08:55:49 -0500

    Adding react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+
+import Home from "./components/home";
+import Aircrafts from "./components/aircrafts";
+import Queue from "./components/queue";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <LinkContainer to="/">
+          <Navbar.Brand>ATCS</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <LinkContainer exact to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/aircrafts">
+              <Nav.Link>Aircrafts</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/queue">
+              <Nav.Link>Queue</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Switch>
+        <Container className="pt-5">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/aircrafts" component={Aircrafts} />
+          <Route exact path="/queue" component={Queue} />
+        </Container>
+      </Switch>
+    </Router>
   );
 }
 
