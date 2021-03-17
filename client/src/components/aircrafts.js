7cdc2f77fb15edcf17ae02e612ea670108baa94c
Mario Yepes <marioy47@gmail.com>
2021-03-17 08:56:04 -0500

    Adding missing components from the last commit

---
+++ client/src/components/aircrafts.js
@@ -0,0 +1,9 @@
+
+const Aircrafts = () => {
+  return (
+    <h1>Aircrafts</h1>
+  );
+}
+
+export default Aircrafts;
+
---
+++ client/src/components/queue.js
@@ -0,0 +1,9 @@
+
+const Queue = () => {
+  return (
+    <h1>Queue</h1>
+  );
+}
+
+export default Queue;
+
